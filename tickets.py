61fe987464932396ec47f76ab2e166bb4d2e880a
cjxh <405019079@qq.com>
2017-07-21 23:58:02 +0800

    tickets_json?

--- tickets.py
+++ tickets.py
@@ -15,11 +15,25 @@ Options:
 '''
 
 from docopt import docopt
+from stations import stations
+import requests
+from pprint import pprint
 
 def cli():
 	"""command-line interface"""
-	arguments = docopt(__doc__)
-	print(arguments)
+	arguments = docopt(__doc__, version='Tickets 1.0')
+	from_station = stations.get(arguments['<from>'],None)
+	to_station = stations.get(arguments['<to>'],None)
+	train_date = arguments['<date>']
+	url = ('https://kyfw.12306.cn/otn/leftTicket/query?'
+		   'leftTicketDTO.train_date={}&'
+		   'leftTicketDTO.from_station={}&'
+		   'leftTicketDTO.to_station={}&'
+		   'purpose_codes=ADULT').format(train_date,from_station,to_station)
+	r = requests.get(url,verify=False)
+	pprint(r.json())
+
+	
 
 if __name__ == '__main__':
 	cli()
